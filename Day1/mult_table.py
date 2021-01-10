501a8c5a7751cdf40d532d1a0ebb550f92d324fd
slakshminands <slakshminands@gmail.com>
2021-01-10 20:20:57 +0000

    multiplication table from 1 to 10 of a given number

---
+++ Day1/mult_table.py
@@ -0,0 +1,9 @@
+num = input("Enter a number: ")
+def mult(num):
+    for i in range(1,11):
+        ret = int(num)*i
+        print(str(num )+" X "+str(i)+"="+str(ret))
+        
+        
+mult(num)
+    
